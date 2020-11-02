9d8f4389209ea71a1a2fabddf2091e16819b2c6c
NIcolas <nicolas.huleux@mail.com>
2020-11-02 20:12:42 +0100

    Setting https://github.com/sequelize/sequelize-auto/ PART 3

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// ------------------------------- 
-// LOAD APP CONFIG ( FTP TOKEN,CLOUDINARY TOKEN,MYSQL TOKEN, etc ...)- ON CHARGE LA CONFIGURATION DE L'APP 
+// -------------------------------
+// LOAD APP CONFIG ( FTP TOKEN,CLOUDINARY TOKEN,MYSQL TOKEN, etc ...)- ON CHARGE LA CONFIGURATION DE L'APP
 // -------------------------------
 const config = require("./config.json");
-
 // ------------------------------
 // LOAD OFFICIAL NODE MODULES - CHARGEMENT DES MODULES NODES ...
 // -------------------------------
@@ -14,59 +13,52 @@ const session = require("express-session");
 const bcrypt = require("bcryptjs");
 const fs = require("fs");
 const logStream = fs.createWriteStream(config.logs_path, { flags: "a" });
-const { Sequelize } = require('sequelize'); // Relational DBS
-const mysql = require('mysql2/promise');
-const system = require('system-commands')
-
+const { Sequelize } = require("sequelize"); // Relational DBS
+const mysql = require("mysql2/promise");
+const system = require("system-commands");
 // -------------------------------
 // THIS IS AN EXPRESS APP
 // -------------------------------
 const app = express();
-
 // -------------------------------
 // MANAGING SERVER PORT - GESTION DES PORTS
 // -------------------------------
 const port = process.env.PORT || 80;
-
 // -------------------------------
-// CORS - GESTION DE LA PROTECTION CORS 
+// CORS - GESTION DE LA PROTECTION CORS
 // -------------------------------
 if (port == 80) {
-    // LOCALHOST AND OPENODE 
+    // LOCALHOST AND OPENODE
     app.use(
         require("cors")({
             origin: function(origin, callback) {
                 callback(null, origin);
             },
-            credentials: true,
+            credentials: true
         })
     );
 } else {
     // AZURE AND HEROKU
     app.use(cors());
 }
-
 // -------------------------------
 // OTHER RELATED PORTS FUNCTIONS  (CONNEXION TO OTHERS DATABASE IF LOCALHOST)
 // -------------------------------
 if (port == 80) {
-    // LOCALHOST AND OPENODE 
+    // LOCALHOST AND OPENODE
 
-    mysql_connect();
     // mysql_crud_routes_generation();
 } else {
-    mysql_connect();
 
-    // It's heroku, so we need this to hide credentials: 
+    // It's heroku, so we need this to hide credentials:
     get_heroku_env_vars();
 }
 // -------------------------------
 // USING SESSIONS - UTILISATION DES SESSIONS
-// -------------------------------             
-app.use(session({ secret: "ssshhhhh", saveUninitialized: true, resave: true }));
-
 // -------------------------------
-// MANAGING FILES AND STATICS DIRECTORIES 
+app.use(session({ secret: "ssshhhhh", saveUninitialized: true, resave: true }));
+// -------------------------------
+// MANAGING FILES AND STATICS DIRECTORIES
 // -------------------------------
 // VUE APP DIRECTORY ( GENERATED WITH NPM RUN BUILD ) // SERVING THE VUE.JS APP
 app.use(serveStatic(__dirname + "/dist"));
@@ -74,15 +66,14 @@ app.use(serveStatic(__dirname + "/dist"));
 app.use(express.static(__dirname + "/tmp"));
 // UPLOADS : FILES STORING DIRECTORY
 app.use(express.static(__dirname + "/tmp/files"));
-
 // -------------------------------
-// MANAGING JSON AND BODY PARSER PARAMS 
+// MANAGING JSON AND BODY PARSER PARAMS
 // -------------------------------
 const bodyParser = require("body-parser");
 app.use(
     bodyParser.urlencoded({
         extended: true,
-        limit: "50mb",
+        limit: "50mb"
     })
 );
 // create application/json parser
@@ -91,29 +82,26 @@ app.use(
     bodyParser.json({
         limit: "50mb",
         inflate: true,
-        strict: false,
+        strict: false
     })
 );
-
-
 // -------------------------------
 // RELATIONAL DATABASES HANDLINGS (MYSQL,SQLITE)
 // -------------------------------
-
 /*
  * Connect mysql using sequelize
- 
     Pool : max :Never have more than five open connections (max: 5)
     min : At a minimum, have zero open connections/maintain no minimum number of connections (min: 0)
     idle: Remove a connection from the pool after the connection has been idle (not been used) for 10 seconds (idle: 10000)
-
  * @params config.json
  * @return none
  * @error  none
  */
-function mysql_connect() {
 
-    const sequelize = new Sequelize(config.mysql.db_name, config.mysql.user, config.mysql.password, {
+const sequelize = new Sequelize(
+    config.mysql.db_name,
+    config.mysql.user,
+    config.mysql.password, {
         dialect: config.mysql.dialect,
         host: config.mysql.host,
         port: config.mysql.port,
@@ -122,47 +110,57 @@ function mysql_connect() {
             min: 0,
             idle: 10000
         }
+    }
+);
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log(
+            "Connection to MYSQL by SEQUELIZE has been established successfully."
+        );
+        import_models();
+    })
+    .catch(err => {
+        console.error("Unable to connect to the database:", err);
     });
 
+/*
+ * Import Generated Sequelize data models. https://github.com/sequelize/sequelize-auto 
+ * @params none
+ * @return none
+ * @error  none
+ */
+function import_models() {
 
+    // Theses models have been generated using https://github.com/sequelize/sequelize-auto 
+    // Example command : sequelize-auto -o "./models" -d sql7374024 -h sql7.freemysqlhosting.net -u sql7374024 -p x -x 932SrSVwjb -e mysql
+    var initModels = require("./models/init-models").initModels;
+    var models = initModels(sequelize);
 
+    // This will become a LOOP - ca va se ransformer en LOOP
+    var customers = models.customers;
+    var orders = models.orders;
 
-    /*  var x = require('./models/init-models.js');
-    x.initModels(sequelize); */
-    /*     var db = {}; */
-    var DataTypes = require('sequelize/lib/data-types');
-    var customers = require('./models/customers.js')(sequelize, DataTypes);
-    /*     db.sequelize = sequelize; //contain a settings of database
-        db.Sequelize = Sequelize;
-
-        module.exports = db; */
-    customers.findAll().then((c) => {
-        console.log(c);
-        console.log('********************')
-
-    }).finally(() => {
-        sequelize.close();
-    });
-
-
-
-
-    sequelize
-        .authenticate()
-        .then(() => {
-            console.log('Connection to MYSQL by SEQUELIZE has been established successfully.');
-
-
+    // Demo Query
+    customers
+        .findAll()
+        .then(c => {
+            console.log(c);
+            console.log("********************");
         })
-        .catch(err => {
-            console.error('Unable to connect to the database:', err);
+        .finally(() => {
+            sequelize.close();
         });
-
-
-
 }
 
-function import_models() {
+
+/*
+ * Generate REST routes from Sequelize data models
+ * @params none
+ * @return none
+ * @error  none
+ */
+function generate_routes() {
 
 
 }
@@ -175,13 +173,18 @@ function import_models() {
  */
 async function mysql_initialize() {
     // create db if it doesn't already exist
-    const token = { "user": config.mysql.user, "password": config.mysql.password, "host": config.mysql.host };
+    const token = {
+        user: config.mysql.user,
+        password: config.mysql.password,
+        host: config.mysql.host
+    };
     const connection = await mysql.createConnection(token);
-    await connection.query('CREATE DATABASE IF NOT EXISTS ' + config.mysql.name + ';');
+    await connection.query(
+        "CREATE DATABASE IF NOT EXISTS " + config.mysql.name + ";"
+    );
     mysql_connect();
-    write_connexion_to_logs()
+    write_connexion_to_logs();
 }
-
 /*
  * Create all Mysql DB Cruds and Routes  automatically from an existing database HIIK !! - See https://github.com/o1lab/xmysql
  * Crée toutes les routes ( Post, Put, Delete, Read) et cruds NODE EXPRESS automatiquement, à partir d'une database MYSQL existante !
@@ -198,17 +201,28 @@ async function mysql_initialize() {
  * @error  none
  */
 async function mysql_crud_routes_generation() {
-    system('xmysql -h ' + config.mysql.host + ' -u ' + config.mysql.user + ' -p ' + config.mysql.password + ' -n 3000  -d ' + config.mysql.name).then(output => {
-            console.log(output)
-            write_connexion_to_logs()
-        }).catch(error => {
-            console.error(error)
+    system(
+            "xmysql -h " +
+            config.mysql.host +
+            " -u " +
+            config.mysql.user +
+            " -p " +
+            config.mysql.password +
+            " -n 3000  -d " +
+            config.mysql.name
+        )
+        .then(output => {
+            console.log(output);
+            write_connexion_to_logs();
         })
-        // -------------------------------
-        // PROXY ALL API ROUTES QUERIES TO PORT 3000 TO USE WITH MYSQL ROUTES GENERATOR https://stackoverflow.com/questions/10435407/proxy-with-express-js
-        // -------------------------------
-    var proxy = require('express-proxy-server');
-    app.use('xmysql/api', proxy('http://localhost:3000/api'));
+        .catch(error => {
+            console.error(error);
+        });
+    // -------------------------------
+    // PROXY ALL API ROUTES QUERIES TO PORT 3000 TO USE WITH MYSQL ROUTES GENERATOR https://stackoverflow.com/questions/10435407/proxy-with-express-js
+    // -------------------------------
+    var proxy = require("express-proxy-server");
+    app.use("xmysql/api", proxy("http://localhost:3000/api"));
 }
 /*
  * Create models if no exist
@@ -219,11 +233,9 @@ async function mysql_crud_routes_generation() {
 async function mysql_models() {
     // init models and add them to the exported db object
     // db.User = require('../users/user.model')(sequelize);
-
     // sync all models with database
     // await sequelize.sync();
 }
-
 /*
  * Connect sqlite using sequelize
  * @params db
@@ -232,25 +244,21 @@ async function mysql_models() {
  */
 function connect_sqlite() {
     const sequelize = new Sequelize({
-        dialect: 'sqlite',
-        storage: 'db/database.sqlite'
+        dialect: "sqlite",
+        storage: "db/database.sqlite"
     });
     sequelize
         .authenticate()
         .then(() => {
-            console.log('Connection to SQLITE has been established successfully.');
+            console.log("Connection to SQLITE has been established successfully.");
         })
         .catch(err => {
-            console.error('Unable to connect to the database:', err);
+            console.error("Unable to connect to the database:", err);
         });
 }
-
-
 // -------------------------------
 // HELPER FUNCTIONS
 // -------------------------------
-
-
 /*
  * Write db connexion to logs file - On journalise la connexion
  * @params none
@@ -263,7 +271,7 @@ function write_connexion_to_logs() {
     // logStream.end(''); TODO
 }
 /*
- * HEROKU ENV VARS : NEEDED TO HIDE ALL CREDENTIALS IN A HEROKU / GITHUB ENV . 
+ * HEROKU ENV VARS : NEEDED TO HIDE ALL CREDENTIALS IN A HEROKU / GITHUB ENV .
  * On récupère nos mots de passe des variables d'environnement stockés sur heroku, sinon, tout le monde verrait nos mots de passe .
  * @params none
  * @return none
