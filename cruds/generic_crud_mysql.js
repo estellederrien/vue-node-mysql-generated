70a465bd4718fa0762aac3898700e185e3f1dd56
NIcolas <nicolas.huleux@mail.com>
2020-11-08 18:41:15 +0100

    Adding a new working generic_crud REST for MYSQL , based on the sequelize-router module. The difference is that it doesn't generate the routes, cause routes are generics . The second diffrence is that it is easier for me to add an advanced middleware to it .
    Ajout d'un CRUD REST générique pour mysql , basé sur le code de sequelize-router !

---
+++ cruds/generic_crud_mysql.js
@@ -0,0 +1,97 @@
+//crud.js https://medium.com/@bvodola/crud-routes-generator-with-node-express-js-mongoose-30a16538e16a
+
+const express = require('express');
+
+module.exports = (model) => {
+
+    // ======
+    // Create
+    // ======
+    const create = (req, res) => {
+
+        model.create(req.body)
+            .then(function(dbModel) {
+                res.json(dbModel);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    };
+
+    // =========
+    // Read many
+    // =========
+    const readMany = (req, res) => {
+        model.findAll({
+                where: req.query
+            })
+            .then(function(dbModel) {
+                res.json(dbModel);
+            })
+            .catch(function(err) {
+                res.json(err);
+            })
+    };
+
+    // ========
+    // Read one
+    // ========
+    const readOne = (req, res) => {
+        model.findOne({
+                where: { id: req.params.id }
+            })
+            .then(function(dbUser) {
+                res.json(dbUser);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    };
+
+    // ======
+    // Update
+    // ======
+    const update = (req, res) => {
+        model.update(req.body, {
+                where: { id: req.params.id }
+            })
+            .then(function(dbModel) {
+                res.json(dbModel);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    };
+
+    // ======
+    // Remove
+    // ======
+    const remove = (req, res) => {
+        model.destroy({
+                where: { id: req.params.id }
+            })
+            .then(function(dbModel) {
+                res.json(dbModel);
+            })
+            .catch(function(err) {
+                res.json(err);
+            });
+    };
+
+
+
+    // ======
+    // Routes
+    // ======
+
+    let router = express.Router();
+
+    router.post('/', create);
+    router.get('/', readMany);
+    router.get('/:_id', readOne);
+    router.put('/:_id', update);
+    router.delete('/:_id', remove);
+
+    return router;
+
+}
